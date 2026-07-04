e5fccca20062c14c8ca516b7574877833564c757
arpit <arpit2003y@gmail.com>
2026-07-04 16:16:39 +0530

    chore(warnings):got_rid_of_the_warnings

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -54,38 +54,39 @@ impl FromStr for ChunkType {
 }
 
 impl ChunkType {
-    fn bytes(&self) -> [u8; 4] {
+    pub fn bytes(&self) -> [u8; 4] {
         self.data_bytes
     }
 
-    fn is_valid(&self) -> bool {
+    pub fn is_valid(&self) -> bool {
         return match self.data_bytes {
             [_, _, b'A'..=b'Z', _] => true,
             _ => false,
         };
     }
 
-    fn is_critical(&self) -> bool {
+    pub fn is_critical(&self) -> bool {
         return match self.data_bytes {
             [b'A'..=b'Z', _, _, _] => true,
             _ => false,
         };
     }
 
-    fn is_public(&self) -> bool {
+    pub fn is_public(&self) -> bool {
         return match self.data_bytes {
             [_, b'A'..=b'Z', _, _] => true,
             _ => false,
         };
     }
-    fn is_reserved_bit_valid(&self) -> bool {
+
+    pub fn is_reserved_bit_valid(&self) -> bool {
         return match self.data_bytes {
             [_, _, b'A'..=b'Z', _] => true,
             _ => false,
         };
     }
 
-    fn is_safe_to_copy(&self) -> bool {
+    pub fn is_safe_to_copy(&self) -> bool {
         return match self.data_bytes {
             [_, _, _, b'a'..=b'z'] => true,
             _ => false,
