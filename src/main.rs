729e4a96a0ed5ca2a53cf212641cfc536f3af500
arpit <arpit2003y@gmail.com>
2026-07-05 08:59:54 +0530

    chore(cli):command_line_setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,68 @@
+use clap::{Args, Parser, Subcommand};
+
+use crate::chunk_type::ChunkType;
+
 pub mod chunk;
 pub mod chunk_err;
 pub mod chunk_type;
 pub mod png;
 
+#[derive(Parser)]
+#[command(
+    name = "pngnator",
+    version = "1.0",
+    about = "A steganography tool for png images."
+)]
+struct Cli {
+    #[arg(short, long, global = true)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Encode(EncodeArgs),
+    Decode(MutateArgs),
+    Remove(MutateArgs),
+    Print { file_path: String },
+}
+
+#[derive(Args)]
+struct EncodeArgs {
+    file_path: String,
+    chunk_type: ChunkType,
+    message: String,
+
+    #[arg(default_value = "output.png")]
+    output_file: String,
+}
+
+#[derive(Args)]
+struct MutateArgs {
+    file_path: String,
+    chunk_type: ChunkType,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Encode(EncodeArgs {
+            file_path,
+            chunk_type,
+            message,
+            output_file,
+        }) => {}
+        Commands::Decode(MutateArgs {
+            file_path,
+            chunk_type,
+        }) => {}
+        Commands::Remove(MutateArgs {
+            file_path,
+            chunk_type,
+        }) => {}
+        Commands::Print { file_path } => {}
+    }
 }
